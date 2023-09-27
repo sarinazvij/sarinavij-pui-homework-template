430f0503f714220748a6802c9e3cc7ebdb3518f5
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-09-26 23:23:48 -0400

    editing code
    editing code

--- solution-hw3/JS/js3.js
+++ solution-hw3/JS/js3.js
@@ -17,7 +17,7 @@ let glazeInfo = {
     }  
 }; 
 
-let packSizeAdapt = {
+let packAdapt = {
     first:{
         name: "1",
         price: 1, 
@@ -34,65 +34,36 @@ let packSizeAdapt = {
         name: "12", 
         price: 10,
     }
-}
-
-console.log(glazeInfo[1]);
+}; 
 
 let basePrice = 2.49; 
-let glazeOption = document.getElementById("glazeoptions");
-//console.log(glazeOption); 
-glazeOption.addEventListener('change', glazeChange); 
+
+
+
+
 
 let packOption = document.getElementById("packsize");
-packOption.addEventListener('change', packChange);  
+packOption.addEventListener('change', packChange, glazeChange);  
 function packChange(event){
-   // glazeChange(); 
 
-   let currentoption = Object.values(packSizeAdapt);
-   console.log(currentoption); 
- 
-   
-
-
-   // let current = packSizeAdapt.price
-   // console.log(current); 
-    //console.log(currentoption); 
-   // console.log(packSizeAdapt[indexOf(current)]); 
-  //  let totalPrice = basePrice + glazeInfo[current]; 
-
-
-   // console.log(totalPrice); 
-//finalprice.innerHTML = totalPrice;
-   
-
-
+   let current = event.target.value; 
+    let totalPrice = basePrice * packAdapt[current].price; 
+    finalprice.innerHTML = totalPrice;
+    //let blah = parseInt(packAdapt[current].price); 
+  return totalPrice;
  }
 
-
-
+ let glazeOption = document.getElementById("glazeoptions");
+ glazeOption.addEventListener('change', glazeChange, packChange); 
 
 function glazeChange(event){
-//     // console.log(Object.values(glazeInfo)[3].price)
-//     // let blah = abc.price; 
-//     // let blah = glazeInfo.abc[1];
-//     // console.log(blah); 
-//     //console.log("did it reach here"+abc);
+ 
 let currentoption = event.target.value;
 console.log(currentoption); 
-let totalPrice = basePrice + glazeInfo[currentoption].price; 
-
-
-finalprice.innerHTML = totalPrice;
-
-return totalPrice; 
-
+let newPrice = basePrice + glazeInfo[currentoption].price; 
+finalprice.innerHTML = newPrice;
 }
 
-
-
-
-
-//finalprice.innerHTML = totalPrice;
 
 
 
@@ -110,32 +81,13 @@ for (let glaze in glazeInfo){
     glazeOption.appendChild(option);
 }
 
-let newPack = document.querySelector("#packsize");
-for (let size in packSizeAdapt){
+//let newPack = document.querySelector("#packsize");
+for (let size in packAdapt){
     let option = document.createElement('option');
-    option.innerHTML =  packSizeAdapt[size].name; 
+    option.innerHTML =  packAdapt[size].name; 
+    option.setAttribute('value', size);
     //console.log(option);
-    newPack.appendChild(option);
+    packOption.appendChild(option);
 }
-
-
-//packSizeAdapt.addEventListener('change', packChange); 
-// function packChange(){
-//     let totalPrice = index + basePrice; 
-//     console.log(totalPrice); 
-// }//
-
-//let total = document.querySelector("#finalprice");
-
-
-
-
-
-// function displayPrice(roll){
-//     let finalPrice = document.getElementById("#pricecartdetail"); 
-//     finalPrice.innerText = roll.cost; 
-// }
-
-
 
 

688fd4ebc2c40a0fa3f85a55851d18ed861cc543
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-09-29 15:53:36 -0400

    updating
    updating

--- solution-hw4/JS/js4.js
+++ solution-hw4/JS/js4.js
@@ -101,6 +101,13 @@ for (let size in packAdapt){
 }
 
 
-
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
 
 

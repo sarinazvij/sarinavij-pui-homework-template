db6ed5340f11451e9b927e948e9e1c7bbdc1defb
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-10-01 22:42:03 -0400

    adding more code
    adding more code

--- solution-hw4/JS/js4.js
+++ solution-hw4/JS/js4.js
@@ -43,21 +43,22 @@ let cart = [];
 const queryString = window.location.search; 
 const params = new URLSearchParams(queryString);
 const rollType = params.get("roll");
-console.log(rollType);
+//console.log(rollType);
 
 
 const headerElement = document.querySelector("#handmade");
 headerElement.innerText = rollType + " Cinnamon Roll"; 
 
 let rollArray = Object.keys(rolls); 
-console.log(rollArray);
+//console.log(rollArray);
 let mainIndex = rollArray.indexOf(rollType); 
-console.log(mainIndex); 
+//console.log(mainIndex); 
 let basePrice = Object.values(rolls)[mainIndex].basePrice; 
 const rollPicture = document.querySelector("#originalimageorder"); 
 rollPicture.src = '../../assets/products/' + Object.values(rolls)[mainIndex].imageFile; 
 
-
+let packSize = "1"
+let rollGlazing = "Keep original"
 
 let glazingPrice = 0;
 let packPrice = 1;
@@ -67,15 +68,21 @@ let packOption = document.getElementById("packsize");
 packOption.addEventListener('change', packChange);  
 
 function packChange(event){
+   
     let current = event.target.value;
+    packSize = packAdapt[current].name;
     packPrice = packAdapt[current].price;
     updateTotalPrice();
+    callRoll(); 
 }
 
 function glazeChange(event){
     let currentoption = event.target.value;
+    //console.log(currentoption); 
     glazingPrice = glazeInfo[currentoption].price;
+    rollGlazing = glazeInfo[currentoption].name;
     updateTotalPrice();
+    callRoll();
 }
 
 function updateTotalPrice() {
@@ -83,7 +90,7 @@ function updateTotalPrice() {
     let eval = (Math.floor(1000 * totalPrice)/1000);
     let newEval = eval.toFixed(2); // This line of code was inspired by the following website: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
     finalprice.innerHTML = newEval; 
-    console.log(totalPrice);
+   // console.log(totalPrice);
 }
 
 for (let glaze in glazeInfo){
@@ -100,14 +107,24 @@ for (let size in packAdapt){
     packOption.appendChild(choice);
 }
 
-
 class Roll {
     constructor(rollType, rollGlazing, packSize, basePrice) {
         this.type = rollType;
-        this.glazing =  rollGlazing;
+        this.glazing = rollGlazing;
         this.size = packSize;
         this.basePrice = basePrice;
     }
 }
 
+function callRoll(){
+    //console.log(packSize); 
+    //console.log(rollGlazing); 
+    let purchase = new Roll(rollType, rollGlazing, packSize, basePrice);
+    cart.push(purchase); 
+    return purchase; 
 
+}
+
+function hello(){
+    console.log(callRoll());
+}

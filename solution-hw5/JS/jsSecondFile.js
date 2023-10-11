167d37e78fdecb1a72b401bd6a0cea7e47fc3496
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-10-11 00:21:44 -0400

    making progress
    making progress

--- solution-hw5/JS/jsSecondFile.js
+++ solution-hw5/JS/jsSecondFile.js
@@ -44,7 +44,9 @@ class Roll2 {
         this.rollPrice = rollPrice;  
     }
 }
+
 let finalCart = new Set ();  
+
 
     let cartMain = new Roll2 ("Original", "Sugar Milk", "1", 2.49)
     let cartMain2 = new Roll2 ("Walnut", "Vanilla Milk", "12", 3.49)
@@ -68,10 +70,58 @@ console.log(findPrice(cartMain));
 console.log(findPrice(cartMain4));
 
 
-function appendThing(){
-    let mainThing = document.querySelector(".cartdescriptions1");
-    cartMain.element 
+function addThing(rollType, rollGlazing, packSize, rollPrice){
+    const sarina = new Roll2(rollType, rollGlazing, packSize, rollPrice);
+    finalCart.add(sarina);
+    return sarina; 
+}
 
+
+
+
+function appendThing(sarina){
+    let template = document.querySelector("#cardtemplate");
+    let clone = template.content.cloneNode(true);
+    sarina.element = clone.querySelector(".cartorder");
+    // const remove = sarina.element.querySelector(".remove");
+    // console.log(remove); 
+    // remove.addEventListener("click", deleteEntry(sarina)); 
+    // //);
+const yikes = document.querySelector(".cartbox"); 
+yikes.prepend(sarina.element);
+addRollInfo(sarina);
+
+}
+
+function addRollInfo(sarina){
+    let rollImageElement = sarina.element.querySelector(".cartimages");
+    let rollNameElement = sarina.element.querySelector(".cartinfo");
+    let rollGlazeElement = sarina.element.querySelector(".cartinfo");
+    let rollSizeElement = sarina.element.querySelector(".cartinfo");
+    let endPriceElement = sarina.element.querySelector(".cartinfoprice");
+
+    //rollImageElement.src = 
+    rollNameElement.innerText = sarina.type;
+    rollGlazeElement.innerText = sarina.glazing; 
+    rollSizeElement.innerText = sarina.size;
+    endPriceElement.innerText = sarina.rollPrice;
+
+}
+
+function deleteEntry(sarina){
+    sarina.element.remove(); 
+    finalCart.delete(sarina);
+}
+
+
+const sup = addThing(cartMain); 
+
+for (const sarina of finalCart){
+console.log(sarina);
+appendThing(sarina); 
 
 
 }
+
+
+

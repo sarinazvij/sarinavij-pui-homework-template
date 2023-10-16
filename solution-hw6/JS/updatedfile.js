62e218a60552c876b463f00f366abecbbc428cc6
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-10-15 23:57:20 -0700

    editing code
    editing code

--- solution-hw6/JS/updatedfile.js
+++ solution-hw6/JS/updatedfile.js
@@ -1,1 +1,31 @@
+class Roll2 {
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing = rollGlazing;
+        this.size = packSize;
+        this.rollPrice = rollPrice;  
+    }
+}
+
+function callRoll(){
+    
+    let purchase = new Roll(rollType, rollGlazing, packSize, basePrice);
+    cart.push(purchase); 
+    return purchase; 
+}
+
+function rollToCart(){
+    console.log(callRoll());
+
+}
+
+function storeRoll (){
+    let cartArray = Array.from(finalCart);
+    console.log(cartArray);
+    let cartArrayUpdate = JSON.parse(cartArray); 
+    console.log(cartArrayUpdate);
+    localStorage.setItem("storedCart", cartArrayUpdate);
+}
+
+
 

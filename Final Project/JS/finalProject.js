25cfeee35a4e4f6637213f7e9989106ea836e2e0
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-11-19 21:22:30 -0500

    d3
    d3

--- Final Project/JS/finalProject.js
+++ Final Project/JS/finalProject.js
@@ -1,1 +1,26 @@
 
+
+// Plot.plot({
+//     marks: [
+//       Plot.ruleY([0]),
+//       Plot.areaY(data, {
+//         x: "x",
+//         y: d => d.y > 10 ? d.y : null,
+//         fill: 'red', fillOpacity: 0.3}),
+//       Plot.lineY(data, {x: "x", y: "y"})
+//     ]
+//   })
+
+
+                     
+         
+var coffeeCup = d3.select("body")
+   .append("svg")
+  //  .attr("height", 1000)
+  //  .attr("width", 500);
+
+coffeeCup.append("polygon")
+   .attr("points", "300,100 250,210 160,210, 100,100")
+   .style("fill", "green")
+   .style("stroke", "black")
+   .style("strokeWidth", "10px");

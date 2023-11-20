47dccc119dbfd316e909fa85ade2eef28adc8106
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-11-20 11:01:49 -0500

    adding info
    adding info

--- Final Project/JS/finalProject.js
+++ Final Project/JS/finalProject.js
@@ -11,16 +11,83 @@
 //     ]
 //   })
 
+// var theCircle = d3.select("#dataviz_buttonEventListener")
+ 
 
-                     
-         
-var coffeeCup = d3.select("body")
-   .append("svg")
+// // A function that update the color circle
+// function changeSize() {
+//   theCircle
+//   .append("svg")
+  
+//   .append("polygon")
+//   .attr("points", "400,100, 235,210, 160,210, 100,100")
+
+// }
+
+// // Add an event listener to the button created in the html part
+// d3.select("#Button").on("input", changeSize )
+
+  
+// function createCup() {
+//    theCircle
+//    .append("tag")
+//    .append("circle")
+
+//    .attr("r", 180)
+//  }
+      
+//  https://d3-graph-gallery.com/graph/interactivity_button.html
+// var coffeeCup = d3.select("#cup")
+// .append("svg")
+// .attr("points", "300,100, 235,210, 160,210, 100,100")
+   
   //  .attr("height", 1000)
   //  .attr("width", 500);
 
-coffeeCup.append("polygon")
-   .attr("points", "300,100 250,210 160,210, 100,100")
-   .style("fill", "green")
-   .style("stroke", "black")
-   .style("strokeWidth", "10px");
+// poly = [{"x":0.0, "y":25.0},
+// {"x":8.5,"y":23.4},
+// {"x":13.0,"y":21.0},
+// {"x":19.0,"y":15.5}]
+
+   // .style("fill", "green")
+   // .style("stroke", "black")
+   // .style("strokeWidth", "10px");
+
+
+
+//    function myFunc() { 
+//       var para = document.getElementById("p"); 
+//       para.classList.toggle("paragraphClass"); 
+//   } 
+
+
+//   var path = function(){
+//    coffeeCup
+//    .remove(); 
+
+//   };
+    
+//     d3.select('#Button').on('click', function ); 
+
+
+
+// if ( path ) {
+   
+//     path.remove();
+//     // Remove dots
+//     path = null;
+// } else {
+//    path=coffeeCup.selectAll("polygon")
+//    .data([poly])
+//    .enter().append("polygon")
+
+//   .attr("stroke","black")
+//   .attr("stroke-width",2);
+  
+   
+
+// }
+
+// });
+
+

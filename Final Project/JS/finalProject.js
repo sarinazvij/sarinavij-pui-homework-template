3858dbafb6325d06f720d16f2e528ded9a1df6da
sarinazvij <143374550+sarinazvij@users.noreply.github.com>
2023-12-08 02:01:07 -0500

    saving
    saving

--- Final Project/JS/finalProject.js
+++ Final Project/JS/finalProject.js
@@ -1,29 +1,25 @@
 
+
+
 let icedCoffeeCup = d3.select("#cup")
 .append("svg")
 .attr("height", 300)
 .attr("width", 500)    
 .attr("viewBox", `0 0 100 900`)
 .attr("preserveAspectRatio", "xMinYMin meet")
-.classed("svg-content", true); 
+.classed("svg-content", true)
 
-
-
-
- 
-
-let r = 200;
+  
 let hotCoffeeCup = d3.select("#cup")
 .append("svg")
-.attr("height", 300)
-.attr("width", 500) 
+.attr("width", 200)
+.attr("height", 200)
+.append("g")
+.attr("transform","translate(250,250)rotate(90)")
 .attr("viewBox", `0 0 100 900`)
 .attr("preserveAspectRatio", "xMinYMin meet")
-.classed("svg-content", true); 
+.classed("svg-content", true)
 
-
-// var grad = hotCoffeeCup.append("defs").append("linearGradient").attr("id", "grad")
-// .attr("x1", "0%").attr("x2", "0%").attr("y1", "100%").attr("y2", "0%");
 
 
 
@@ -44,27 +40,33 @@ function cupType(element){
     .attr("stroke", "black"); 
   }
 
-else if (element.value === "Hot"){
- 
   d3.select("body").select("svg").remove(); // aided by https://stackoverflow.com/questions/46499721/d3-js-graphs-not-getting-toggled-on-click-of-radio-buttons
-  hotCoffeeCup = d3.select("#cup")
-  .append("svg");
+
+  else if (element.value === "Hot"){
+ 
   
+  hotCoffeeCup = d3.select("#cup")
+  .append("svg")
+  .append("g")
 
-var grad = hotCoffeeCup.append("defs").append("linearGradient").attr("id", "grad")
-.attr("x1", "0%").attr("x2", "0%").attr("y1", "100%").attr("y2", "0%");
+  var arc = d3.arc()
+  .innerRadius(0)
+  .outerRadius(150)
+  .startAngle(0)
+  .endAngle(Math.PI);
+  
+  hotCoffeeCup.append("path")
+  .style("fill", "white")
+  .attr("d", arc());
 
-  grad.append("stop").attr("offset", "50%").style("stop-color", "white");
-grad.append("stop").attr("offset", "50%").style("stop-color", "#D2F6F9");
-
-// grad.append("stop").attr("offset", "30%").style("stop-color", "lightblue");
-// grad.append("stop").attr("offset", "30%").style("stop-color", "white");
-
-hotCoffeeCup.append("circle")
-  .attr("cx", r)
-  .attr("cy", r)
-  .attr("r", r)
-  .attr("fill", "url(#grad)");
+  
+   hotCoffeeCup.append("rect")
+   .attr("height", 200)
+   .attr("width", 300)
+      .attr("transform","translate(0,-150)rotate(90)")
+      .style("fill", "white"); 
+   
+   
 
 }
 }
@@ -80,7 +82,6 @@ function addCoffee(element){
     if ((element.value === "Light") && (iceCheck.value === "Iced")){
       icedCoffeeCup
       .append("polygon")  
-      // 
       .attr("points", "537,350 500,500 325,500 286,350")
       .style("fill", "#C3890C");
     }
@@ -97,29 +98,33 @@ function addCoffee(element){
       .style("fill", "#401D01");
     }
     else if ((element.value === "Light") && (iceCheck.value === "Hot")){
-      d3.select("body").select("svg").remove(); // aided by https://stackoverflow.com/questions/46499721/d3-js-graphs-not-getting-toggled-on-click-of-radio-buttons
-      hotCoffeeCup = d3.select("#cup")
-      .append("svg");
-      var grad = hotCoffeeCup.append("defs").append("linearGradient").attr("id", "grad")
-      .attr("x1", "0%").attr("x2", "0%").attr("y1", "100%").attr("y2", "0%");
-    
-
-      grad.append("stop").attr("offset", "20%").style("stop-color", "#C3890C");
-
-  
-      grad.append("stop").attr("offset", "10%").style("stop-color", "white");
-
-      
-      
-      grad.append("stop").attr("offset", "50%").style("stop-color", "#D2F6F9");
-      
-
-    
-    hotCoffeeCup.append("circle")
+      // d3.select("body").select("svg").remove(); // aided by https://stackoverflow.com/questions/46499721/d3-js-graphs-not-getting-toggled-on-click-of-radio-buttons
+      // hotCoffeeCup = d3.select("#cup")
+      // .append("svg");
+      hotCoffeeCup
+      .append("circle")
       .attr("cx", r)
       .attr("cy", r)
       .attr("r", r)
-      .attr("fill", "url(#grad)");
+      .attr("fill", "orange"); 
+     
+      
+      // var grad = hotCoffeeCup.append("defs").append("linearGradient").attr("id", "grad")
+      // .attr("x1", "0%").attr("x2", "0%").attr("y1", "100%").attr("y2", "0%");
+    
+
+      // grad.append("stop").attr("offset", "20%").style("stop-color", "#C3890C");
+
+  
+      // grad.append("stop").attr("offset", "10%").style("stop-color", "white");
+
+      
+      
+      // grad.append("stop").attr("offset", "50%").style("stop-color", "#D2F6F9");
+      
+
+      // hotCoffeeCup
+      // .attr("fill", "url(#grad)");
     
     }
   
@@ -207,3 +212,9 @@ function addSyrup(){
 
 }
 
+
+
+
+
+
+ 
